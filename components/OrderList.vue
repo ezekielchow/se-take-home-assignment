662d8cce96e92f59723cd0a78a44d52ab7f9362b
Ezekiel <ezekielchow94@gmail.com>
2024-02-04 22:05:47 +0800

    Added statistics for orders

--- components/OrderList.vue
+++ components/OrderList.vue
@@ -23,8 +23,11 @@ const getQueue = () => {
       v-bind:key="q.id"
       class="border-slate-600 border-2 rounded my-2"
     >
-      <div class="flex p-2">
-        {{ q.name }}
+      <div class="flex flex-col p-2">
+        <div class="font-bold">{{ q.name }}</div>
+        <div v-if="q.bot !== undefined">
+          {{ `By Chef: ${String(q.bot.id).padStart(3, "0")}` }}
+        </div>
       </div>
     </div>
   </div>
